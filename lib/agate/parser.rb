89e9a851343f0bcf23cf1fe9a271de2d2ccedf56
Jesse B. Hannah <jesse@jbhannah.net>
2013-03-05 00:09:13 -0700

    Use "constants" instead of class variables

--- lib/agate/parser.rb
+++ lib/agate/parser.rb
@@ -2,15 +2,15 @@
 module Agate
   class Parser
     # Default options
-    @@defaults = {
+    DEFAULTS = {
       :delimiters => "【】"
     }
 
     # Regexp reserved characters to escape when matching
-    @@reserved = ["(", ")", "[", "]", "{", "}", ".", ",", "+", "*"]
+    RESERVED = ["(", ")", "[", "]", "{", "}", ".", ",", "+", "*"]
 
     def initialize(options = {})
-      @options = @@defaults.merge(options)
+      @options = DEFAULTS.merge(options)
     end
 
     # Parse `text` and return it with ruby character markup
@@ -18,8 +18,8 @@ module Agate
       first = @options[:delimiters].chars.first
       last  = @options[:delimiters].chars.last
 
-      first = /#{'\\' + first}/ if @@reserved.include? first
-      last  = /#{'\\' + last}/  if @@reserved.include? last
+      first = /#{'\\' + first}/ if RESERVED.include? first
+      last  = /#{'\\' + last}/  if RESERVED.include? last
 
       text.gsub(/(\p{Han}+)(#{first})([\p{Hiragana}\p{Katakana}]+)(#{last})/,
         '<ruby>\1<rp>\2</rp><rt>\3</rt><rp>\4</rp></ruby>')
