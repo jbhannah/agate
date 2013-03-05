a50a86b0f9f5e2731a871de18ca3952fa4677ef8
Jesse B. Hannah <jesse@jbhannah.net>
2013-03-04 21:50:40 -0700

    Test multiple parsings in a single string

--- spec/lib/parser_spec.rb
+++ spec/lib/parser_spec.rb
@@ -2,9 +2,9 @@
 require "spec_helper"
 
 describe Agate::Parser do
-  let!(:text) { "勉強【べんきょう】します" }
+  let!(:text) { "勉【べん】強【きょう】します" }
   let!(:agate) { Agate::Parser.new(text) }
-  let!(:parsed_text) { "<ruby>勉強<rt>べんきょう</rt></ruby>します" }
+  let!(:parsed_text) { "<ruby>勉<rt>べん</rt></ruby><ruby>強<rt>きょう</rt></ruby>します" }
 
   it "saves text to an instance variable" do
     agate.text.should eql(text)
